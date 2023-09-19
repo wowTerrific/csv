5a967045e03cc57403685bc1c7e2f2d511a09f3f
marshall <marshallstutzman@gmail.com>
2023-09-19 15:19:42 -0400

    working on parsing them records better

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,7 @@
 use super::{Record, errors, Result};
 
+
+// TODO: make sure anything within "" is contained within a field...
 pub fn raw_csv_to_records(raw: &str) -> Result<Vec<Record>> {
     if !raw.contains(',') || !raw.contains('\n') {
         return Err(
@@ -14,9 +16,16 @@ pub fn raw_csv_to_records(raw: &str) -> Result<Vec<Record>> {
     // TODO: Check first line for 'sep=<char>' and use
     // that as the split!
     for line in raw.lines() {
+
+        // TODO:
+        // find "", make sure that is stays as a string...
+        // if line starts with ", append remaining line to last record in data...
+        // let record = parse_string_to_record(line);
+
         let record: Record = line.split(',')
                                 .map(|item| item.to_string())
                                 .collect();
+        
         data.push(record);
     }
     Ok(data)
@@ -56,10 +65,50 @@ pub fn path_validate(path: &str) {
 }
 
 
+fn parse_string_to_record(raw_string: &str) -> Record {
+    let mut record: Record = Vec::new();
+    let mut in_quotes = false;
+    let mut current = String::new();
+
+    for c in raw_string.chars() {
+        if c == '"' {
+            in_quotes = !in_quotes;
+            continue;
+        } else if c == ',' && !in_quotes {
+            if current.len() > 0 {
+                record.push(current);
+                current = String::new();
+                continue;
+            }
+        }
+
+        current.push(c);
+    }
+
+    if current.len() > 0 {
+        record.push(current);
+    }
+
+    record
+}
+
+
 #[cfg(test)]
-mod tests {
+mod utils_tests {
     use crate::Record;
     use super::*;
+
+    #[test]
+    fn test_parse_string_to_record() {
+        let ut = "\"one,one,one\",two,three";
+        let expected: Record = vec![
+            String::from("one,one,one"), 
+            String::from("two"), 
+            String::from("three")
+        ];
+
+        assert_eq!(expected, parse_string_to_record(ut));
+    }
 
     #[test]
     fn utils_records_to_string() {
@@ -95,6 +144,19 @@ mod tests {
         let csv_string = String::from("one,two,three\nfour,five,six\nseven,eight,nine");
 
         let result = raw_csv_to_records(&csv_string).unwrap_or(vec![vec!["FAIL".to_string()]]);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn utils_parse_csv_with_quotes_newlines_into_records() {
+        let expected: Vec<Record> = vec![
+            vec![String::from("one,one,one"), String::from("two"), String::from("three")],
+            vec![String::from("four"), String::from("fi\nve"), String::from("six")],
+            vec![String::from("s,e,v\ne,n"), String::from("eight"), String::from("nine")],
+        ];
+
+        let csv_string = String::from("\"one,one,one\",two,three\nfour,\"fi\nve\",six\n\"s,e,v\ne,n\",eight,nine");
+        let result = raw_csv_to_records(&csv_string).expect("failed to parse quotes and newlines into to CSV");
         assert_eq!(expected, result);
     }
 }
